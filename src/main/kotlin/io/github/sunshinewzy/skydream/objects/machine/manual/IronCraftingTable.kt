c2ad31a4b75597efb1e0ecc6e77356a61e00b530
Sunshine-wzy <1123574549@qq.com>
2021-02-26 22:03:08 +0800

    更新 IronCraftingTable.kt

--- src/main/kotlin/io/github/sunshinewzy/skydream/objects/machine/manual/IronCraftingTable.kt
+++ src/main/kotlin/io/github/sunshinewzy/skydream/objects/machine/manual/IronCraftingTable.kt
@@ -6,7 +6,10 @@ import io.github.sunshinewzy.sunstcore.modules.machine.SMachineRunEvent
 import io.github.sunshinewzy.sunstcore.modules.machine.SMachineSize
 import io.github.sunshinewzy.sunstcore.modules.machine.SMachineStructure
 import io.github.sunshinewzy.sunstcore.objects.SBlock
-import io.github.sunshinewzy.sunstcore.objects.inventoryholder.SInventoryHolder
+import io.github.sunshinewzy.sunstcore.objects.SItem
+import io.github.sunshinewzy.sunstcore.objects.inventoryholder.SPartProtectInventoryHolder
+import io.github.sunshinewzy.sunstcore.objects.orderWith
+import io.github.sunshinewzy.sunstcore.utils.createEdge
 import org.bukkit.Bukkit
 import org.bukkit.Material
 
@@ -34,11 +37,22 @@ object IronCraftingTable : MachineManual(
         Triple(0, 2, 0)
     )
 ) {
-    private val holder = SInventoryHolder(name)
+    private val holder = SPartProtectInventoryHolder(
+            ArrayList<Int>().apply {
+                for (i in 2..4) {
+                    for (j in 2..4) {
+                        add(i orderWith j)
+                    }
+                }
+                add(7 orderWith 3)
+            },
+            name
+    )
 
     
     override fun manualRun(event: SMachineRunEvent.Manual) {
         val inv = Bukkit.createInventory(holder, 5 * 9, "铁制工作台")
+        inv.createEdge(5, SItem(Material.STAINED_GLASS_PANE))
         
         
     }
